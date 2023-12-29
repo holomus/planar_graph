8329b915a4ccef371e2c8b80a23019dd141faba3
holomus <leoabrahaam@gmail.com>
2023-12-29 19:09:01 +0500

    restore

--- .github/workflows/changelog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the range of commits between the previous tag and the current tag
-CHANGELOG=$(git log --pretty=format:"* [%s] by %an in %h" $(git describe --tags --abbrev=0 @^)..@)
-
-# Print the generated changelog
-echo "## What's Changed"
-echo "$CHANGELOG"
